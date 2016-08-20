23254923900b4db28b3efcf4540fa3ed5c564c42
pieroit <piero.savastano@gmail.com>
2016-08-20 17:27:00 +0200

    add tf*idf vectorizer

--- clustering/bbcluster.py
+++ clustering/bbcluster.py
@@ -1,13 +1,44 @@
 import random
 import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def cluster(docs):
-    num = 10
-    hashes = ['C'+str(random.getrandbits(128)) for i in range(1,num+1)]
+
+    # data preparation
+    vectorizer = TfidfVectorizer(tokenizer=tokenizeAndClean, max_features=1000)
+    corpus = []
     for doc in docs:
-        doc["cluster"] = random.choice(hashes)
+        corpus.append(doc['title'])
+    vectorizedCorpus = vectorizer.fit_transform(corpus)
+
+    print vectorizer.get_feature_names()
+
+    print corpus[0], vectorizer.inverse_transform(vectorizedCorpus[0])
+
+
+    # run k-means with different nCluster and random seed
+    #for nCluster in range(4,20):
+    #    for seed in range(0,10):
+
+            # run k-means
+            #print nCluster, seed
+
+            # store inertia and clusters
+
+    # take best run of k-means (minimal inertia)
+
+    # assign each document a cluster and yield
+    for doc in docs:
+        doc["cluster"] = 23
         yield doc
 
+def tokenizeAndClean(text):
+
+    # substitute all numbers with a special token
+
+    # substitute all FUUUUUUUUUUCK
+
+    return text.split(' ')
 
 if __name__ == '__main__':
 
@@ -23,4 +54,4 @@ if __name__ == '__main__':
 
     # cluster the docs
     for d in cluster(docs):
-        print d
+        2#print d
