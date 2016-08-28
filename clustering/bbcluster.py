e4e4d8c5bd3bb8fa8219d58b262b5247edf2d849
pieroit <piero.savastano@gmail.com>
2016-08-28 18:39:14 +0200

    three different approaches

--- clustering/bbcluster.py
+++ clustering/bbcluster.py
@@ -1,35 +1,60 @@
+
 import random
 import csv
-from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.cluster import KMeans
+from sklearn.decomposition import NMF, LatentDirichletAllocation
 
 def cluster(docs):
 
     # data preparation
-    vectorizer = TfidfVectorizer(tokenizer=tokenizeAndClean, max_features=1000)
+    #vectorizer = TfidfVectorizer(tokenizer=tokenizeAndClean, min_df=0.01, max_df=0.5, max_features=1000)
+    vectorizer = CountVectorizer(tokenizer=tokenizeAndClean, min_df=0.01, max_df=0.5, max_features=1000)
     corpus = []
     for doc in docs:
         corpus.append(doc['title'])
     vectorizedCorpus = vectorizer.fit_transform(corpus)
 
-    print vectorizer.get_feature_names()
-
-    print '\n', vectorizedCorpus[0],corpus[0], vectorizer.inverse_transform(vectorizedCorpus[0])
-
+    # print vocabulary
+    #for i, w in enumerate(vectorizer.get_feature_names()):
+    #    print w, vectorizer.idf_[i]
 
     # run k-means with different nCluster and random seed
-    for nCluster in range(4,10):
+    for nCluster in range(4, 10):
 
         print '\n clusters:', nCluster
 
-        for seed in range(0,3):
+        for seed in range(0,1):
 
-            # run k-means
+            ######### k-means ###########
             km = KMeans(n_clusters=nCluster, random_state=seed)
             km.fit(vectorizedCorpus)
 
             # store inertia and clusters
             print '\t seed:', seed, '\t-->', km.inertia_
+            #for centroid in km.cluster_centers_:
+            #    print '\t\t', getMostImportantWords(centroid, vectorizer)
+            #############################
+
+            ######### matrix factorization ###########
+            #mf = NMF(n_components=nCluster, random_state=seed)
+            #mf = LatentDirichletAllocation(n_topics=nCluster, random_state=seed)
+            #mf.fit(vectorizedCorpus)
+
+            # store inertia and clusters
+            #print '\t seed:', seed#, '\t-->', mf.reconstruction_err_
+            #for comp in mf.components_:
+            #    print '\t\t', getMostImportantWords(comp, vectorizer)
+            #############################
+
+            ######## examples ###########
+            #print '=============='
+            #for i, doc in enumerate(corpus):
+            #    print doc
+            #    print mf.transform(vectorizedCorpus[i]), '\n'
+            #print '==============\n'
+
 
     # take best run of k-means (minimal inertia)
 
@@ -40,11 +65,30 @@ def cluster(docs):
 
 def tokenizeAndClean(text):
 
-    # substitute all numbers with a special token
+    # all lowercase
+    text = text.lower()
 
-    # substitute all FUUUUUUUUUUCK
+    # take away punctuation and numbers
+    symbols = string.punctuation + '1234567890'
+    for symbol in symbols:
+        text = text.replace(symbol, ' ')
 
-    return text.split(' ')
+    tokens = text.split(' ')
+
+    #return tokens
+    return filter(lambda w: len(w) > 4, tokens)
+
+def getMostImportantWords(centroid, vectorizer):
+
+    labels = vectorizer.get_feature_names()
+
+    weightedWords = {}
+    for i, weight in enumerate(centroid):
+        word = labels[i]
+        weightedWords[word] = weight
+
+    bestWords = sorted(weightedWords, key=weightedWords.__getitem__)
+    return bestWords[:5]
 
 if __name__ == '__main__':
 
